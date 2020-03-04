8cefcd08f496068af12093e1d5e0b52ca35f7bfa
Robert Brajcich <robertbrajcich@gmail.com>
2020-03-03 19:51:10 -0800

    Added project structure and basic build script

---
+++ client/cpp/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+BASE_DIR=$(dirname $0)
+
+cd $BASE_DIR
+
+# Delete build directories if they already exist
+if [ -d obj ]; then rm -rf obj; fi
+if [ -d release ]; then rm -rf release; fi
+
+# Make build output directories
+mkdir obj
+mkdir release
+
+# Navigate to obj dir
+cd obj
+
+# Build object files and output in obj/ directory
+g++ -c -I ../src ../src/*.cpp 
+
+# Return to base dir
+cd ..
+
+# Create static lib in release/lib dir
+mkdir release/lib 
+ar -cvq release/lib/p2p-relay.a obj/*.o
+
+# Clone client public API headers to release/include
+mkdir release/include
+cp -R include/p2p-relay release/include
---
+++ client/cpp/include/p2p-relay/Client.h
@@ -0,0 +1,10 @@
+#ifndef P2P_RELAY__CLIENT_H
+#define P2P_RELAY__CLIENT_H
+
+namespace P2PRelay {
+
+    void printHelloMessage();
+
+} // namespace P2PRelay
+
+#endif
