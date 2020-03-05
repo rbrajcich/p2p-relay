ea0ebe31ff717d53a4b2546c75612d89c59eb757
rbrajcich <robertbrajcich@gmail.com>
2020-03-04 22:48:40 -0800

    feat: Added project structure and basic build scripts
    * Added project structure and basic build script
    
    * Added server build script

---
+++ client/cpp/src/HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+namespace P2PRelay {
+
+    void printHelloMessage() {
+        std::cout << "Hello World!" << std::endl;
+    }
+
+}
---
+++ server/src/HelloServer.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello From Server!" << std::endl;
+    return 0;
+}
