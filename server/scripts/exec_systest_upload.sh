435c8f033cc8b0c465e485efed066bdfea32fd65
Robert Brajcich <robertbrajcich@gmail.com>
2020-03-15 14:27:53 -0700

    fix: Add logs to help solve future coverage upload failures

--- server/scripts/exec_systest_upload.sh
+++ server/scripts/exec_systest_upload.sh
@@ -4,4 +4,5 @@ BASE_DIR=$(dirname $0)
 cd "${BASE_DIR}/../.."
 
 # Upload the server_systest.info lcov file to codecov.io
+echo "Uploading the server systest lcov file"
 bash <(curl -s https://codecov.io/bash) -f lcov/server_systest.info
